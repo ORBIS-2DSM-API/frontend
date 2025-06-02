aee0eb7b5317ff22286e96d84e49471eb0391fc8
KwMajor <majorhydra@protonmail.com>
2025-06-02 10:35:56 -0300

    feat: Adjust display settings on form pages and base template for final presentation

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <>
       <Header />
-      <main className="h-screen bg-gradient-to-b from-gray-100 to-gray-200">
+      <main className="h-screen pt-6 bg-gradient-to-b from-gray-100 to-gray-200">
         <Outlet />
       </main>
     </>
--- src/pages/formularioEmpresa.jsx
+++ src/pages/formularioEmpresa.jsx
@@ -259,7 +259,7 @@ function FormularioEmpresa() {
   };
 
   return (
-    <div className="max-w-2xl h-full mx-auto mt-10 p-6 bg-white rounded-md shadow-md overflow-auto">
+    <div className="max-w-2xl h-full mx-auto pt-8 p-6 bg-white rounded-md shadow-md overflow-scroll">
       <h1 className="text-3xl font-semibold text-gray-800 mb-8 text-center">
         Formulário para {nomeEmpresa.replace('-', ' ').toUpperCase()}
       </h1>
